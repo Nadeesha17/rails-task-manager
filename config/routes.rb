e37b85056b19bd79edaa015bb99d21435dea1997
Nadeesha Illamperuma <nadisliit@gmail.com>
2025-03-04 12:52:49 +0100

    rails new

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get 'tasks', to: 'tasks#index', as: :tasks  # List all tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_task  # Form for a new task
+  get 'tasks/:id', to: 'tasks#show', as: :task # Show a specific task
+  post 'tasks', to: 'tasks#create'  # Handle new task submission
+   # Routes for editing/updating
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # Form to edit a task
+  patch 'tasks/:id', to: 'tasks#update' # Handle task update
+    # Route for deleting a task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
+  resources :tasks
+  # Defines the root path route ("/")
+   root "tasks#index"
+  # root "posts#index"
+end
